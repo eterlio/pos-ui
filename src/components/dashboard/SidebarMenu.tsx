123446666f884a3d92860155d072b5cf9f5a9e42
bernardarhia <barhia@infonaligy.com>
2024-05-12 18:19:31 +0000

    added dashboard design

--- src/components/dashboard/SidebarMenu.tsx
+++ src/components/dashboard/SidebarMenu.tsx
@@ -8,14 +8,14 @@ interface SidebarMenuProps {
 const SidebarMenu: React.FC<SidebarMenuProps> = ({ title, routesData }) => {
   return (
     <div className="my-5">
-      <p className="section-title  pl-3 text-[0.655rem]">{title}</p>
+      <p className="section-title  pl-3 text-[0.655rem] text-white font-bold">{title}</p>
       <ul>
         {routesData &&
           routesData.length &&
           routesData.map((data, index) => {
             return (
               <SidebarMenuItem
-                icon={<data.icon size={19} />}
+                icon={<data.icon size={19} className="group-hover:text-green-400"/>}
                 title={data.title}
                 url={data.url}
                 key={index}
--- src/pages/shared/Home.tsx
+++ src/pages/shared/Home.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 
 const Home = () => {
+
   return (
     <div className="min-h-screen flex items-center justify-center">
       <Link to="/auth/login" className="min-h-8 py-2 px-4 min-w-52 text-center text-white bg-primaryButton rounded">Login</Link>
