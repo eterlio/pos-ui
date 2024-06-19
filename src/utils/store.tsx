95b4aaa458ca2aa6cb948c9680954e8d71ced352
bernardarhia <barhia@infonaligy.com>
2024-06-19 10:04:36 +0000

    added notifications to store

--- src/utils/store.tsx
+++ src/utils/store.tsx
@@ -1,22 +1,52 @@
+import { NotificationProps } from "@/interfaces/notification";
 import { AuthUserResponse } from "@/interfaces/users";
-import { createContext, useState } from 'react';
+import { createContext, useState } from "react";
+
 export type StoreContextProps = {
   saveAuthUser: (user: AuthUserResponse) => void;
   authUser: AuthUserResponse | null;
-  clearAuthUser: ()=> void
+  clearAuthUser: () => void;
+  notifications: NotificationProps[] | null;
+  setNotificationData: (notification: NotificationProps) => void;
 };
+
 export const StoreContext = createContext<StoreContextProps | null>(null);
 
-export const StoreProvider: React.FC<{children: React.ReactNode}> = ({ children }) => {
+export const StoreProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const [authUser, setAuthUser] = useState<AuthUserResponse | null>(null);
+  const [notifications, setNotifications] = useState<NotificationProps[] | null>(null);
+
   const saveAuthUser = (currentAuthUser: AuthUserResponse) => {
-    setAuthUser((previousUser)=>{
-      return {...previousUser, ...currentAuthUser};
+    setAuthUser((previousUser) => {
+      return { ...previousUser, ...currentAuthUser };
     });
   };
+
   const clearAuthUser = () => {
     setAuthUser(null);
-  }
+  };
 
-  return <StoreContext.Provider value={{ saveAuthUser, authUser, clearAuthUser }}>{children}</StoreContext.Provider>;
+  const setNotificationData = (notification: NotificationProps) => {
+    setNotifications((prevNotifications) => {
+      if (prevNotifications && prevNotifications.length) {
+        return [...prevNotifications, notification];
+      } else {
+        return [notification];
+      }
+    });
+  };
+
+  return (
+    <StoreContext.Provider
+      value={{
+        saveAuthUser,
+        authUser,
+        clearAuthUser,
+        notifications,
+        setNotificationData
+      }}
+    >
+      {children}
+    </StoreContext.Provider>
+  );
 };
